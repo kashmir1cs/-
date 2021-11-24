8c823bacb13615f75de04045f3dfe0618d99fe96
RASCALCS <63025037+kashmir1cs@users.noreply.github.com>
2021-11-24 23:42:42 +0900

    Create 기본예제풀이01.py

---
+++ 예제풀어보기/작업형1번/기본예제풀이01.py
@@ -0,0 +1,22 @@
+# 출력을 원할 경우 print() 함수 활용
+# 예시) print(df.head())
+
+# getcwd(), chdir() 등 작업 폴더 설정 불필요
+# 파일 경로 상 내부 드라이브 경로(C: 등) 접근 불가
+
+# 데이터 파일 읽기 예제
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler #sklearn의 MinMax Scaler 사용
+df = pd.read_csv('data/mtcars.csv', index_col=0) #dataframe 작성
+df_qsec=df[['qsec']] 
+# 사용자 코딩
+scaler_minmax=MinMaxScaler()
+scaler_minmax.fit(df_qsec)
+qsec_scaled=scaler_minmax.transform(df_qsec)
+record_overhalf=len(qsec_scaled[qsec_scaled>0.5]) #0.5보다 큰 값 저장하기
+print("레코드 수량: ",record_overhalf)
+
+
+# 답안 제출 예시
+print("레코드 수량: ",record_overhalf)
+# print(평균변수값)
